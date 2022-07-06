d6fecc5a5ee57ad06e84433d0d16e6480fb273a8
Nirupama03 <94469160+Nirupama03@users.noreply.github.com>
2022-07-06 16:03:35 +0530

    Add files via upload

---
+++ linearsearch.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+int x;
+void linear(int a[],int x);
+int n;
+int i;
+
+void main()
+{
+	int a[100];
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+		a[i]=rand()%100;
+	}
+	printf("The array elements are :\n");
+	for (i=0;i<n;i++)
+	{
+		printf("%d\t",a[i]);
+	}
+	printf("\n");
+	printf("Enter the number you have to search :");
+	scanf("%d",&x);
+	linear(a,x);
+}
+
+void linear (int a[], int  x)
+{
+	int flag=0;
+	for (i=0;i<n;i++)
+	{
+		if (a[i]==x)
+		{
+			flag =1;
+			break;
+		}
+	}
+	if (flag ==1)
+	{
+		printf("%d found at position %d\n",x,i);
+	}
+	else
+	{
+		printf("Element not found!\n");
+	}
+}
